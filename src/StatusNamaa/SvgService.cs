87486dbb5b7fa5c331f8998d11807046c4c8f101
ctyar <ctyar@outlook.com>
2025-06-27 10:30:52 +0100

    Change SVG sizex to px and make the height dynamic

--- src/StatusNamaa/SvgService.cs
+++ src/StatusNamaa/SvgService.cs
@@ -22,7 +22,7 @@ internal sealed class SvgService
     {
         var svgDoc = new StringBuilder();
 
-        AddHeader(svgDoc);
+        AddHeader(svgDoc, metrics);
 
 #if DEBUG
         metrics[0].Value = 100;
@@ -36,17 +36,18 @@ internal sealed class SvgService
         return svgDoc.ToString();
     }
 
-    private static void AddHeader(StringBuilder svgDoc)
+    private static void AddHeader(StringBuilder svgDoc, List<MetricDisplayItem> metrics)
     {
-        svgDoc.AppendLine("""
-            <svg xmlns="http://www.w3.org/2000/svg" style="background:#20242c;font-family:'Segoe UI',sans-serif;" width="620px" height="280px" viewBox="0 0 620 280">
-            <text x="1rem" y="4rem" fill="#bfc9d1" font-size="4rem" font-weight="500">Status Namaa</text>
+        var height = 36 + 10 + ((metrics.Count + 1) * 24) + 10;
+        svgDoc.AppendLine($"""
+            <svg xmlns="http://www.w3.org/2000/svg" style="background:#20242c;font-family:'Segoe UI',sans-serif;" width="480px" height="{height}px" viewBox="0 0 480 {height}">
+            <text x="10px" y="36px" fill="#bfc9d1" font-size="36px" font-weight="500">Status Namaa</text>
             """);
     }
 
-    private void AddBody(StringBuilder svgDoc, List<MetricDisplayItem> metrics)
+    private static void AddBody(StringBuilder svgDoc, List<MetricDisplayItem> metrics)
     {
-        svgDoc.AppendLine("""<g font-size="2rem" font-weight="400">""");
+        svgDoc.AppendLine("""<g font-size="24px" font-weight="400">""");
 
         for (var i = 0; i < metrics.Count; i++)
         {
@@ -66,7 +67,7 @@ internal sealed class SvgService
 
     private static void AddMetricName(StringBuilder svgDoc, MetricDisplayItem metric, int rowIndex)
     {
-        svgDoc.AppendLine($"""<text x="1rem" y="{7 + rowIndex * 2}rem" fill="#53b1fd">{metric.Name}</text>""");
+        svgDoc.AppendLine($"""<text x="10px" y="{84 + rowIndex * 24}px" fill="#53b1fd">{Truncate(metric.Name)}</text>""");
     }
 
     private static void AddMetricBars(StringBuilder svgDoc, MetricDisplayItem metric, int rowIndex)
@@ -82,7 +83,7 @@ internal sealed class SvgService
                 break;
             }
 
-            svgDoc.AppendLine($"""<rect x="{18 + 1.5 * i}rem" y="{6 + rowIndex * 2}rem" width="1rem" height="1rem" rx="0.2rem" fill="{color}"/>""");
+            svgDoc.AppendLine($"""<rect x="{10 + (17 * 12) + 20 * i}px" y="{(60 + 24 - 16) + rowIndex * 24}px" width="16px" height="16px" rx="2px" fill="{color}"/>""");
         }
 
         svgDoc.AppendLine("</g>");
@@ -90,7 +91,7 @@ internal sealed class SvgService
 
     private static void AddMetricValue(StringBuilder svgDoc, MetricDisplayItem metric, int rowIndex)
     {
-        svgDoc.AppendLine($"""<text x="33rem" y="{7 + rowIndex * 2}rem" fill="{GetColor(metric.Value)}">""" +
+        svgDoc.AppendLine($"""<text x="{10 + (17 * 12) + (20 * 10)}px" y="{84 + rowIndex * 24}px" fill="{GetColor(metric.Value)}">""" +
             $"{string.Format(metric.Format, metric.Value)}</text>");
     }
 
@@ -110,5 +111,15 @@ internal sealed class SvgService
         }
 
         return Colors.Last().Item2;
+    }
+
+    public static string Truncate(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return value;
+        }
+
+        return value.Length <= 16 ? value : value[..16];
     }
 }
