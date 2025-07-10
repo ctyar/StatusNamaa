f5cdf6d4a4cdffd9529b1e94fbc995bcd37e39be
ctyar <ctyar@outlook.com>
2025-07-10 08:45:38 +0100

    Change the folder structure

--- src/samples/AppHost/Program.cs
+++ src/samples/AppHost/Program.cs
@@ -1,5 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.ApiService>("apiservice");
+var apiService = builder.AddProject<Projects.Api>("apiservice");
 
 builder.Build().Run();
