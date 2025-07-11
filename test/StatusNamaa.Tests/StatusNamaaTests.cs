ae1fdaafc239e04a58f4f17a0abc0bc1744b955a
ctyar <ctyar@outlook.com>
2025-07-11 08:03:01 +0100

    Fix the unit tests

--- test/StatusNamaa.Tests/StatusNamaaTests.cs
+++ test/StatusNamaa.Tests/StatusNamaaTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace StatusNamaa.Tests;
@@ -48,37 +49,21 @@ public class StatusNamaaTests
             </g>
             <g>
             <g clip-path="url(#clip1)"><text x="10px" y="132px" fill="#53b1fd">ThreadPool Queue</text></g>
-            <text x="460px" y="132px" fill="#b0fd6a" text-anchor="end">25</text>
+            <text x="460px" y="132px" fill="#b0fd6a" text-anchor="end">5</text>
             </g>
             <g>
             <g clip-path="url(#clip1)"><text x="10px" y="156px" fill="#53b1fd">Lock Contentions</text></g>
-            <text x="460px" y="156px" fill="#b0fd6a" text-anchor="end"></text>
+            <text x="460px" y="156px" fill="#b0fd6a" text-anchor="end">5</text>
             </g>
             </g>
-            <text x="10px" y="180px" fill="#53b1fd" font-size="10px">Environment: <tspan fill="#b0fd6a">Production</tspan>  Version: <tspan fill="#b0fd6a">0.6.0-alpha.6+f50922c5e0</tspan></text>
+            <text x="10px" y="180px" fill="#53b1fd" font-size="10px">Environment: <tspan fill="#b0fd6a">Production</tspan>  Version: <tspan fill="#b0fd6a">1.2.3-alpha.6+f50922c5e0</tspan></text>
             </svg>
             """;
         var builder = WebApplication.CreateSlimBuilder();
         builder.WebHost.UseTestServer();
-        builder.Services.AddStatusNamaa(o =>
-        {
-            o.Metrics[0].Selector = services =>
-            {
-                return Task.FromResult((double?)100);
-            };
-            o.Metrics[1].Selector = services =>
-            {
-                return Task.FromResult((double?)70);
-            };
-            o.Metrics[2].Selector = services =>
-            {
-                return Task.FromResult((double?)25);
-            };
-            o.Metrics[3].Selector = services =>
-            {
-                return Task.FromResult((double?)null);
-            };
-        });
+        builder.Services.AddStatusNamaa();
+        builder.Services.AddSingleton<ICustomMetricService, MockCustomMetricService>();
+        builder.Services.AddSingleton<IListenerService, MockListenerService>();
 
         var app = builder.Build();
         app.MapStatusNamaa();
@@ -100,7 +85,7 @@ public class StatusNamaaTests
                 <rect x="10px" y="10px" width="180px" height="84px"/>
             </clipPath>
             <text x="10px" y="36px" fill="#bfc9d1" font-size="36px" font-weight="500">Status Namaa</text>
-            <text x="10px" y="84px" fill="#53b1fd" font-size="10px">Environment: <tspan fill="#b0fd6a">Production</tspan>  Version: <tspan fill="#b0fd6a">0.6.0-alpha.6+f50922c5e0</tspan></text>
+            <text x="10px" y="84px" fill="#53b1fd" font-size="10px">Environment: <tspan fill="#b0fd6a">Production</tspan>  Version: <tspan fill="#b0fd6a">1.2.3-alpha.6+f50922c5e0</tspan></text>
             </svg>
             """;
         var builder = WebApplication.CreateSlimBuilder();
@@ -109,6 +94,7 @@ public class StatusNamaaTests
         {
             o.Metrics.Clear();
         });
+        builder.Services.AddSingleton<ICustomMetricService, MockCustomMetricService>();
 
         var app = builder.Build();
         app.MapStatusNamaa();
@@ -136,7 +122,7 @@ public class StatusNamaaTests
             <text x="460px" y="84px" fill="#b0fd6a" text-anchor="end">123</text>
             </g>
             </g>
-            <text x="10px" y="108px" fill="#53b1fd" font-size="10px">Environment: <tspan fill="#b0fd6a">Production</tspan>  Version: <tspan fill="#b0fd6a">0.6.0-alpha.6+f50922c5e0</tspan></text>
+            <text x="10px" y="108px" fill="#53b1fd" font-size="10px">Environment: <tspan fill="#b0fd6a">Production</tspan>  Version: <tspan fill="#b0fd6a">1.2.3-alpha.6+f50922c5e0</tspan></text>
             </svg>
             """;
         var builder = WebApplication.CreateSlimBuilder();
@@ -149,10 +135,11 @@ public class StatusNamaaTests
                 DisplayName = null,
                 Selector = services =>
                 {
-                    return Task.FromResult((double?)123);
+                    return Task.FromResult<double?>(123);
                 }
             });
         });
+        builder.Services.AddSingleton<ICustomMetricService, MockCustomMetricService>();
 
         var app = builder.Build();
         app.MapStatusNamaa();
@@ -180,7 +167,7 @@ public class StatusNamaaTests
             <text x="460px" y="84px" fill="#b0fd6a" text-anchor="end">-1</text>
             </g>
             </g>
-            <text x="10px" y="108px" fill="#53b1fd" font-size="10px">Environment: <tspan fill="#b0fd6a">Production</tspan>  Version: <tspan fill="#b0fd6a">0.6.0-alpha.6+f50922c5e0</tspan></text>
+            <text x="10px" y="108px" fill="#53b1fd" font-size="10px">Environment: <tspan fill="#b0fd6a">Production</tspan>  Version: <tspan fill="#b0fd6a">1.2.3-alpha.6+f50922c5e0</tspan></text>
             </svg>
             """;
         var builder = WebApplication.CreateSlimBuilder();
@@ -192,10 +179,11 @@ public class StatusNamaaTests
             {
                 Selector = services =>
                 {
-                    return Task.FromResult((double?)-1);
+                    return Task.FromResult<double?>(-1);
                 }
             });
         });
+        builder.Services.AddSingleton<ICustomMetricService, MockCustomMetricService>();
 
         var app = builder.Build();
         app.MapStatusNamaa();
